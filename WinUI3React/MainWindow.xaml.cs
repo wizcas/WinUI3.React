0932b3a876dc6ff028477bf2d97ca48d3167b004
Wizcas Chen <chen@0x1c.dev>
2023-01-08 16:08:50 +0800

    copy web builds to the output dir

--- WinUI3React/MainWindow.xaml.cs
+++ WinUI3React/MainWindow.xaml.cs
@@ -30,10 +30,5 @@ namespace WinUI3React
         {
             this.InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
     }
 }
