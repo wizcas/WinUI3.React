f819a529903b21def05060ae6e175ae3f123773c
Wizcas Chen <chen@0x1c.dev>
2023-01-08 16:46:51 +0800

    auto build and copy webapp files

--- WinUI3React/Web/WebHost.cs
+++ WinUI3React/Web/WebHost.cs
@@ -2,12 +2,13 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Text;
 
 namespace WinUI3React.Web;
 
 public class WebHost
 {
-    const int DEFAULT_PORT= 8728;
+    const int DEFAULT_PORT = 8728;
     static readonly string[] HOSTS = new[] {
         "http://localhost",
         "http://127.0.0.1",
@@ -17,7 +18,7 @@ public class WebHost
     private int _port;
     private string _baseDir;
 
-    public WebHost(int port=DEFAULT_PORT)
+    public WebHost(int port = DEFAULT_PORT)
     {
         _port = port;
         _baseDir = Path.Combine(AppContext.BaseDirectory, "webapp");
@@ -49,7 +50,7 @@ public class WebHost
         if (!_server.IsListening) return;
 
         var ctx = _server.EndGetContext(result);
-        var localPath = ctx.Request.Url?.LocalPath.TrimStart('/')??string.Empty;
+        var localPath = ctx.Request.Url?.LocalPath.TrimStart('/') ?? string.Empty;
         var filePath = GetResourcePath(localPath);
         if (string.IsNullOrEmpty(localPath) || !File.Exists(filePath))
         {
@@ -57,11 +58,20 @@ public class WebHost
         }
 
         var resp = ctx.Response;
-        var mimeType = MimeTypeMap.GetMimeType(Path.GetExtension(filePath).TrimStart('.'));
-        resp.Headers.Set("Content-Type", mimeType);
-        var buf = File.ReadAllBytes(filePath);
-        resp.ContentLength64 = buf.Length;
-        resp.OutputStream.Write(buf);
+        if (File.Exists(filePath))
+        {
+            var mimeType = MimeTypeMap.GetMimeType(Path.GetExtension(filePath).TrimStart('.'));
+            resp.Headers.Set("Content-Type", mimeType);
+            var buf = File.ReadAllBytes(filePath);
+            resp.ContentLength64 = buf.Length;
+            resp.OutputStream.Write(buf);
+        }
+        else
+        {
+            resp.StatusCode = (int)HttpStatusCode.NotFound;
+            resp.Headers.Set("Content-Type", "text/plain");
+            resp.OutputStream.Write(Encoding.UTF8.GetBytes("Resource not found."));
+        }
         resp.Close();
 
         Receive();
